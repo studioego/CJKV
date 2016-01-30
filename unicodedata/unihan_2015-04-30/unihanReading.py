cc8197bd5cbfc1163037eb5557b2fcc921a0dfef
DaeHyun Sung <sungdh86+git@gmail.com>
2016-01-30 20:53:54 +0900

    Read Unihan_Readings.txt file using python3
    Python3코드를 이용하여 Unicode의 Unihan Library8.0에 있는 Unihan_Readings.txt파일을
    읽어들이는 코드를 작성함.

---
+++ unicodedata/unihan_2015-04-30/unihanReading.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# read Unihan database txt file 
+# Author: DaeHyun Sung(sungdh86+git@gmail.com)
+def chomp(s):
+    return s[:-1] if s.endswith('\n') else s
+
+def trim_newlines(slist):
+    for i in range(len(slist)):
+        slist[i] = chomp(slist[i])
+    return slist
+
+if __name__ == '__main__':
+    uniHanReadingFile = open('Unihan_Readings.txt','r')
+    while True:
+        itemLine = uniHanReadingFile.readline()
+        if not itemLine: 
+            break
+        if len(itemLine) < 1:
+            continue
+        elif itemLine[0] == '' or itemLine[0] == '#' or itemLine[0] == '\n':
+            continue
+        else:
+            #chomps(itemLine)
+            items = itemLine.split('\t')
+            trim_newlines(items)
+            if len(items) != 3: 
+                print("parsing error: ", items)
+                break
+            print("CodePoint: %s|field: %s|content: %s" % (items[0], items[1], items[2]))
+            
+    uniHanReadingFile.close()
